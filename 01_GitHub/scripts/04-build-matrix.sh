a634ad9eae4c60b231bec2db527b44235c6dc974
iquasere <maildosequeira@gmail.com>
2026-06-09 13:00:52 +0200

    Now they should fail

--- 01_GitHub/scripts/04-build-matrix.sh
+++ 01_GitHub/scripts/04-build-matrix.sh
@@ -8,7 +8,7 @@ echo "Counting matches into summary matrix"
 
 # GROUP 4 - WRITE HERE THE COMMAND TO RUN THE PYTHON SCRIPT THAT BUILDS THE SUMMARY MATRIX
 # THE PYTHON SCRIPT TAKES THE RESULTS DIRECTORY AS A PARAMETER
-python 01_GitHub/scripts/build_matrix.py "$RESULTS_DIR"
+
 
 # check if the matrix file was created successfully
 if [ ! -f "$RESULTS_DIR/matches_summary.csv" ]; then
--- 01_GitHub/scripts/05-build-report.sh
+++ 01_GitHub/scripts/05-build-report.sh
@@ -8,7 +8,7 @@ echo "Generating MultiQC report"
 
 # GROUP 5 - WRITE HERE THE COMMAND TO RUN MULTIQC
 # THE MULTIQC COMMAND SHOULD TAKE THE RESULTS DIRECTORY AS INPUT, THE MULTIQC CONFIG FILE AS CONFIG, AND OUTPUT THE REPORT TO $RESULTS_DIR/multiqc_report
-multiqc "$RESULTS_DIR" -c "$MULTIQC_CONFIG" -o "$RESULTS_DIR"/multiqc_report
+
 
 # check if multiqc_report directory was generated, and exit code 1 if not
 if [ ! -d "$RESULTS_DIR/multiqc_report" ]; then
